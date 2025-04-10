c701da8b9f04b8e3b3170eff75226faabd1c014e
malik201702 <kilamedazsixab@gmail.com>
2025-04-10 19:08:46 +0400

    html.64

---
+++ app.js
@@ -0,0 +1,9 @@
+let eded = prompt("Bir ədəd daxil edin:");
+
+if (eded > 0) {
+  console.log("Müsbət ədəd");
+} else if (eded < 0) {
+  console.log("Mənfi ədəd");
+} else {
+  console.log("Ədəd sıfırdır");
+}
